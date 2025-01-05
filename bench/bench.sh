f151c30be7901828a6bf247f493baa28d847a132
DefTruth <qiustudent_r@163.com>
2025-01-05 20:48:49 +0800

    add bench.sh

---
+++ bench/bench.sh
@@ -0,0 +1,12 @@
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 320 
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 384
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 448
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 512
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 576
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 640
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 704
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 768
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 832
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 896
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 960
+python3 ffpa.py --B 1 --H 48 --N 8192 --show-all --D 1024
