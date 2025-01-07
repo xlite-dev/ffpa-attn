b10bd82be02c6c1e67a2287035d417b0d5e72418
DefTruth <31974251+DefTruth@users.noreply.github.com>
2025-01-07 06:13:04 +0000

    format

--- csrc/pybind/faster_prefill_attn_api.cc
+++ csrc/pybind/faster_prefill_attn_api.cc
@@ -1,21 +1,15 @@
-#include <torch/types.h>
 #include <torch/extension.h>
+#include <torch/types.h>
 
 #define STRINGFY(str) #str
 #define TORCH_BINDING_COMMON_EXTENSION(func) \
   m.def(STRINGFY(func), &func, STRINGFY(func));
 
-void ffpa_mma_acc_f16_L1(torch::Tensor Q, 
-                         torch::Tensor K, 
-                         torch::Tensor V, 
-                         torch::Tensor O, 
-                         int stages);
+void ffpa_mma_acc_f16_L1(torch::Tensor Q, torch::Tensor K, torch::Tensor V,
+                         torch::Tensor O, int stages);
 
-void ffpa_mma_acc_f32_L1(torch::Tensor Q, 
-                         torch::Tensor K, 
-                         torch::Tensor V, 
-                         torch::Tensor O, 
-                         int stages);
+void ffpa_mma_acc_f32_L1(torch::Tensor Q, torch::Tensor K, torch::Tensor V,
+                         torch::Tensor O, int stages);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   TORCH_BINDING_COMMON_EXTENSION(ffpa_mma_acc_f16_L1)
