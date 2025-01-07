0f1fb16fb4a080f447069110fc5d45920ff81410
DefTruth <31974251+DefTruth@users.noreply.github.com>
2025-01-07 15:50:01 +0800

    Update install.sh

--- .dev/install.sh
+++ .dev/install.sh
@@ -1,3 +1,4 @@
+rm -rf $(find . -name __pycache__)
 python3 setup.py bdist_wheel && cd dist # build pyffpa from sources
 python3 -m pip install pyffpa-*-linux_x86_64.whl # pip uninstall pyffpa -y
 cd .. && rm -rf build *.egg-info
