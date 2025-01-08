b6bd71187f3c48ab7770427b0b94be9ee35eddff
DefTruth <31974251+DefTruth@users.noreply.github.com>
2025-01-08 02:42:04 +0000

    update package name -> cuffpa-py

--- .dev/install.sh
+++ .dev/install.sh
@@ -1,4 +1,5 @@
 rm -rf $(find . -name __pycache__)
-python3 setup.py bdist_wheel && cd dist # build pyffpa from sources
-python3 -m pip install pyffpa-*-linux_x86_64.whl # pip uninstall pyffpa -y
-cd .. && rm -rf build *.egg-info
+python3 setup.py bdist_wheel && cd dist # build cuffpa-py from sources
+python3 -m pip install cuffpa-py-*-linux_x86_64.whl # pip uninstall cuffpa-py -y
+cd .. && rm -rf build *.egg-info 
+rm -rf $(find . -name __pycache__)
