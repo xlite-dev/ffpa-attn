a94dd0cb4f1a6d46775879ea71ebf44eae09bb9a
DefTruth <31974251+DefTruth@users.noreply.github.com>
2025-01-13 11:01:46 +0800

    Update install.sh

--- .dev/install.sh
+++ .dev/install.sh
@@ -1,5 +1,5 @@
 rm -rf $(find . -name __pycache__)
-python3 setup.py bdist_wheel && cd dist # build cuffpa-py from sources
-python3 -m pip install cuffpa_py-*-linux_x86_64.whl # pip uninstall cuffpa-py -y
+python3 setup.py bdist_wheel && cd dist 
+python3 -m pip install *.whl 
 cd .. && rm -rf build *.egg-info 
 rm -rf $(find . -name __pycache__)
