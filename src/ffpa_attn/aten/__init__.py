6d2a93f51ddf1a61f2a8ead1aa1efd81736d53e4
DefTruth <qiustudent_r@163.com>
2026-05-07 05:51:53 +0000

    API: align the ffpa_attn_func api with SDPA

--- src/ffpa_attn/aten/__init__.py
+++ src/ffpa_attn/aten/__init__.py
@@ -1,8 +1,8 @@
 """Aten flash-attention forward/backward wrappers for small-D (D <= 256)."""
 
-from .efficient_bwd import _aten_efficient_attn_backward
 from ._flash_bwd import _aten_flash_attn_backward
 from ._flash_fwd import _aten_flash_attn_forward
+from ._efficient_bwd import _aten_efficient_attn_backward
 
 __all__ = [
   "_aten_flash_attn_forward",
